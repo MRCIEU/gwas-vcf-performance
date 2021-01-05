d480b6c94e60fa90304aa0cd6106fbf9c921e199
Matt Lyon <ml18692@bristol.ac.uk>
2021-01-05 12:56:22 +0000

    increased font size

--- combined_plot.R
+++ combined_plot.R
@@ -240,7 +240,7 @@ p <- ggplot(all, aes(x=tool, y=mean, ymin=lower, ymax=upper, fill=File)) +
     geom_errorbar(width = 0.08, position = position_dodge(0.8)) +
     facet_wrap(~ test + analysis, scale="free_y", ncol=6) +
     scale_y_continuous(breaks=pretty_breaks(n=5)) +
-    theme_light() +
+    theme_light(text = element_text(size=12)) +
     scale_fill_brewer(palette = "Dark2") +
     ggtitle("Query performance of GWAS-VCF and unindexed TSV using a range of common operations") +
     xlab("Method") +
